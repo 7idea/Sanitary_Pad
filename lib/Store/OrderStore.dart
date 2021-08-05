5a5ff6dcde8ab64ee8be4ca9a6e24530a7a6018f
Pae <75965035+7idea@users.noreply.github.com>
2021-08-05 14:10:16 +0700

    _sum = _basket[i].qty * _basket[i].price; เปลี่ยนเป็น _sum += _basket[i].qty * _basket[i].price;

--- lib/Store/OrderStore.dart
+++ lib/Store/OrderStore.dart
@@ -12,32 +12,24 @@ class OrderStore extends ChangeNotifier {
   createBasketToOrderStore(List<Product> p) {
     _sum = 0;
     _basket = p;
-    print('createBasketToOrderStore $_basket');
     for (int i = 0; i < _basket.length; i++) {
-      _sum = _basket[i].qty * _basket[i].price;
+      _sum += _basket[i].qty * _basket[i].price;
     }
-    print('sum $_sum');
     notifyListeners();
   }
 
   createAddress(Address a) {
     _address = a;
-    print('createAddress $_address');
     notifyListeners();
   }
 
   createOrder() {
-    print('createOrder --> basket $_basket');
-    print('createOrder --> address $_address');
-
     if (_basket.length > 0 && _address != null) {
       _order = OrderModel(
         basket: _basket,
         address: _address,
         sum: _sum,
       );
-      print('createOrder --> order $_order');
-      print('--------------------------------');
     }
     notifyListeners();
   }
